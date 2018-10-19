ded2033575f2d82b268ff626f1c029cef62cc71c
grant smith <grantsmith1256@gmail.com>
2018-10-19 05:07:00 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
-      
-
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => {},
       }, 
     :capulet => {
-      #your key/value pairs here!
+       :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => {},
       }}
-
-  
 end
